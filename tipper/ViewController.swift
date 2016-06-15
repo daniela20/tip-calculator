c7e9bab9997dbc9073dbd1af4bfcb496c1d78e36
Daniela Gonzalez <danielagonzalez@fb.com>
2016-06-14 18:21:19 -0700

    idk

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var tipLabel: UILabel!
-    
     @IBOutlet weak var totalLabel: UILabel!
     
-    @IBOutlet weak var billField: UITextField!
-    
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var splitTwo: UILabel!
+    
+    @IBOutlet weak var splitThree: UILabel!
+    
+    @IBOutlet weak var splitFour: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,12 +36,17 @@ class ViewController: UIViewController {
         let bill = Double(billField.text!) ?? 0
         let percent = [0.18, 0.2, 0.25]
         let tip = bill * percent[tipControl.selectedSegmentIndex]
-        let total = bill + tip;
+        let total = bill + tip
         
-        tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        splitTwo.text = String(format: "$%.2f", total/2.0)
+        
+        splitThree.text = String(format: "$%.2f", total/3.0)
+        
+        splitFour.text = String(format: "$%.2f", total/4.0)
     }
-
+    
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
